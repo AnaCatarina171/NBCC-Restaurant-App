1e1e8ed51f34b8087aef89c2ae8891ae54c2b3fb
Jose-Silva-01 <zekaaccarlos@gmail.com>
2022-11-05 17:40:16 -0300

    Merge to create a new branch for me :P

--- src/main/java/ca/nbcc/restapp/controller/MenuController.java
+++ src/main/java/ca/nbcc/restapp/controller/MenuController.java
@@ -1,5 +1,22 @@
 package ca.nbcc.restapp.controller;
 
-public class MenuController {
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class MenuController {
+	
+	private ApplicationContext ctx;
+
+	public MenuController(ApplicationContext ctx) {
+		super();
+		this.ctx = ctx;
+	}
+	
+	
+	@GetMapping("/customerMenuDisplay")
+	public String toCustomerMenuDisplay() {
+		
+	}
 }
