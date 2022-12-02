99611e4cef52422e942247674803762d2e6b1d66
Ana Silva <anaaccatarina@gmail.com>
2022-12-02 13:22:07 -0400

    Floor Plan

--- src/main/java/ca/nbcc/restapp/repo/ReservationJpaRepo.java
+++ src/main/java/ca/nbcc/restapp/repo/ReservationJpaRepo.java
@@ -10,18 +10,22 @@ import ca.nbcc.restapp.model.Reservation;
 
 public interface ReservationJpaRepo extends JpaRepository<Reservation, Long>{
 	
-	String customQueryPastRes = "select r from Reservation r where r.date < CURRENT_DATE order by r.date";
-	String customQueryPastResDesc = "select r from Reservation r where r.date < CURRENT_DATE order by r.date desc";
-	String customQueryFutureRes = "select r from Reservation r where r.date = CURRENT_DATE order by r.date";
-	String customQueryFutureResDesc = "select r from Reservation r where r.date >= CURRENT_DATE order by r.date desc";
+	String customQueryPastRes = "select r from Reservation r where r.date < CURRENT_DATE-1 order by r.date";
+	String customQueryPastResDesc = "select r from Reservation r where r.date < CURRENT_DATE-1 order by r.date desc";
+	String customQueryFutureRes = "select r from Reservation r where r.date > CURRENT_DATE-1 order by r.date";
+	String customQueryTodayRes = "select r from Reservation r where r.date > CURRENT_DATE-1 AND r.date < CURRENT_DATE+1  order by r.date";
+	String customQueryFutureResDesc = "select r from Reservation r where r.date > CURRENT_DATE-1 order by r.date desc";
 
 	List<Reservation> findByOrderByDateAscTimeDesc();
 	
-	//@Query(customQueryFutureRes)
+	@Query(customQueryFutureRes)
 	List<Reservation> findByOrderByDate();
 	
-	//@Query(customQueryFutureResDesc)
+	@Query(customQueryFutureResDesc)
 	List<Reservation> findByOrderByDateDesc();
+	
+	@Query(customQueryTodayRes)
+	List<Reservation> findTodayRes();
 	
 	@Query(customQueryPastRes)
 	List<Reservation> findPastReservationsDate();
